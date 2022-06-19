7f604c04c99f45bdfce82c399cf1bcb50f6f4438
verified <65849237+Bitbot25@users.noreply.github.com>
2022-06-19 19:30:54 +0200

    Refactored IR and changed to unions.

--- tuplan-ir/src/lib.rs
+++ tuplan-ir/src/lib.rs
@@ -1,48 +1,187 @@
-use std::{io::{Cursor, Write, Read}, borrow::Cow};
+//! Tuplan IR specification
+//! 
+//! `pushu64` - `(inline val: u64)` Pushes `val` onto the stack.
+//! `pop` - Pops any value off of the stack.
+//! `ret` - `(stack retptr: u32)` Returns to `retptr`.
+//! `goto` - `(inline loc: u32)` Moves the instruction pointer to `loc`.
+//! `addu64` - `(stack a: u64), (stack b: u64)` Pushes a new `u64` onto the stack which is the result of adding `a` and `b`.
+//! `peeku64` - `(stack val: u64)` Displays a u64 to stdout without popping it off the stack.
 
-pub const INST_PUSH_U64: u8 = 0;
-pub const INST_POP: u8 = 1;
-pub const INST_ADD_U64: u8 = 2;
-pub const INST_DUMP_U64: u8 = 3;
+use std::ops::Index;
+use std::mem;
+use disc::disc;
+pub use disc::FromDiscriminant;
 
-// FIXME: Using a std::io::Cursor here is not neccessarily what we want. When we call write the pos is increased by one. Simplified: We cannot read what we write; only what it was initialized with.
-pub type ByteStream = Cursor<Vec<u8>>;
+#[disc]
+pub enum Inst {
+    PushU64,
+    Pop,
+    Ret,
+    Goto,
+    AddU64,
+    PeekU64,
+}
 
+pub struct ByteStream {
+    bytes: Vec<u8>,
+    index: usize,
+}
+
+impl ByteStream {
+    #[inline]
+    #[cold]
+    pub fn new() -> ByteStream {
+        ByteStream { bytes: Vec::new(), index: 0 }
+    }
+
+    #[inline]
+    #[cold]
+    pub fn new_with_bytes(bytes: Vec<u8>) -> ByteStream {
+        ByteStream { bytes, index: 0 }
+    }
+
+    #[inline]
+    pub fn push(&mut self, byte: u8) {
+        self.bytes.push(byte);
+    }
+
+    pub fn read_byte(&mut self) -> Option<u8> {
+        if self.index >= self.bytes.len() {
+            return None;
+        }
+        let byte = unsafe { *self.bytes.get_unchecked(self.index) };
+        self.index += 1;
+        Some(byte)
+    }   
+
+    #[must_use]
+    pub fn read_into_const<const N: usize>(&mut self, buf: &mut [u8; N]) -> bool {
+        if N > self.bytes.len() {
+            return false;
+        }
+
+        *buf = unsafe { self.bytes[self.index..self.index+N].try_into().unwrap_unchecked() };
+        self.index += N;
+        return true;
+    }
+
+    pub fn peek_byte(&mut self) -> Option<u8> {
+        if self.index >= self.bytes.len() {
+            return None;
+        }
+        unsafe { Some(*self.bytes.get_unchecked(self.index)) }
+    }
+
+    pub fn peek_into<'a>(&'a mut self, buf: &mut &'a [u8]) -> bool {
+        let n = buf.len();
+        if n > self.bytes.len() {
+            return false;
+        }
+
+        *buf = &self.bytes[self.index..self.index+n];
+        return true;
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn len(&self) -> usize {
+        self.bytes.len()
+    }
+
+    #[inline]
+    #[must_use]
+    pub fn get(&self, index: usize) -> Option<u8> {
+        match self.bytes.get(index) {
+            Some(v) => Some(*v),
+            None => None,
+        }
+    }
+
+    #[inline]
+    pub fn jump_unchecked(&mut self, index: usize) {
+        self.index = index;
+    }
+}
+
+impl Index<usize> for ByteStream {
+    type Output = u8;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.bytes[index]
+    }
+}
+
+#[inline]
+#[must_use]
+#[cold]
 pub fn bytestream() -> ByteStream {
-    Cursor::new(Vec::with_capacity(32))
+    ByteStream::new()
 }
 
-pub fn bytestream_with(bytes: &[u8]) -> ByteStream {
-    Cursor::new(bytes.to_vec())
+#[inline]
+#[must_use]
+#[cold]
+pub fn bytestream_with(bytes: Vec<u8>) -> ByteStream {
+    ByteStream::new_with_bytes(bytes)
 }
 
-pub fn disassemble(mut bytes: &[u8]) -> String {
-    let mut acc = String::with_capacity(bytes.len() * 8);
-    while bytes.len() > 0 {
-        let (cont, disassembly) = disassemble_single(bytes);
-        acc.push_str(&disassembly);
-        bytes = &bytes[cont..];
+#[must_use]
+pub fn disassemble(bytes: &ByteStream) -> String {
+    let mut buf = String::with_capacity(bytes.len() * 2);
+    let mut index = 0;
+    while index < bytes.len() {
+        index = disassemble_one(bytes, index, &mut buf);
+        buf.push('\n');
     }
-    acc
+    buf
 }
 
-pub fn disassemble_single(bytes: &[u8]) -> (usize, Cow<str>) {
+pub fn disassemble_one(bytes: &ByteStream, start: usize, buffer: &mut String) -> usize {
     #[inline(always)]
-    fn simple_instruction(name: &str) -> (usize, Cow<str>) {
-        (1, Cow::Borrowed(name))
+    fn simple(str: &str, index: usize, buf: &mut String) -> usize {
+        buf.push_str(&*format!("{index} | {str}"));
+        index + 1
     }
 
-    match bytes[0] {
-        INST_PUSH_U64 => { 
-            let mut u64_bytes = [0; 8];
-            Read::read_exact(&mut &bytes[1..9], &mut u64_bytes).expect("Invalid bytecode sequence.");
-            let value = u64::from_le_bytes(u64_bytes);
+    let header = bytes.get(start).expect("Invalid bytecode: Expected instruction header.");
+    let inst: Inst = Inst::from_discriminant(header).expect("Invalid bytecode: Expected instruction header.");
 
-            (9, Cow::Owned(format!("PUSH_U64 {}", value)))
+    match inst {
+        Inst::PushU64 => {
+            let (val, index) = get_u64(bytes, start + 1);
+            buffer.push_str(&*format!("{start} | pushu64 {val}"));
+            index
         },
-        INST_POP => simple_instruction("POP"),
-        INST_ADD_U64 => simple_instruction("ADD_U64"),
-        INST_DUMP_U64 => simple_instruction("DUMP_U64"),
-        _ => unimplemented!("Unknown instruction: {}", bytes[0]),
+        Inst::Pop => simple("pop", start, buffer),
+        Inst::Ret => simple("ret", start, buffer),
+        Inst::Goto => {
+            let (val, index) = get_u32(bytes, start + 1);
+            buffer.push_str(&*format!("{start} | goto {val}"));
+            index
+        },
+        Inst::AddU64 => simple("addu64", start, buffer),
+        Inst::PeekU64 => simple("peeku64", start, buffer),
     }
+}
+
+fn get_u32(bytes: &ByteStream, start: usize) -> (u32, usize) {
+    let mut u32_b = [0; mem::size_of::<u32>()];
+    u32_b[0] = bytes[start];
+    u32_b[1] = bytes[start+1];
+    u32_b[2] = bytes[start+2];
+    u32_b[3] = bytes[start+3];
+    (u32::from_le_bytes(u32_b), start + mem::size_of::<u32>())
+}
+
+fn get_u64(bytes: &ByteStream, start: usize) -> (u64, usize) {
+    let mut u64_b = [0; mem::size_of::<u64>()];
+    u64_b[0] = bytes[start];
+    u64_b[1] = bytes[start+1];
+    u64_b[2] = bytes[start+2];
+    u64_b[3] = bytes[start+3];
+    u64_b[4] = bytes[start+4];
+    u64_b[5] = bytes[start+5];
+    u64_b[6] = bytes[start+6];
+    u64_b[7] = bytes[start+7];
+    (u64::from_le_bytes(u64_b), start + mem::size_of::<u64>())
 }
--- tuplan-vm/examples/bytecode.rs
+++ tuplan-vm/examples/bytecode.rs
@@ -1,34 +1,34 @@
-#![feature(cursor_remaining)]
-
-use tuplan_ir::{INST_PUSH_U64, INST_DUMP_U64, INST_ADD_U64, disassemble};
+use tuplan_ir::{Inst, disassemble};
 use tuplan_vm::Vm;
 
 fn main() {
-    let bytes_10 = u64::to_le_bytes(10);
-    let bytes_20 = u64::to_le_bytes(20);
-    let bytecode = tuplan_ir::bytestream_with(&[
-        INST_PUSH_U64,
-        bytes_10[0],
-        bytes_10[1],
-        bytes_10[2],
-        bytes_10[3],
-        bytes_10[4],
-        bytes_10[5],
-        bytes_10[6],
-        bytes_10[7],
-        INST_PUSH_U64,
-        bytes_20[0],
-        bytes_20[1],
-        bytes_20[2],
-        bytes_20[3],
-        bytes_20[4],
-        bytes_20[5],
-        bytes_20[6],
-        bytes_20[7],
-        INST_ADD_U64,
-        INST_DUMP_U64,
+    let bytecode = tuplan_ir::bytestream_with(vec![
+        Inst::PushU64 as u8,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        Inst::PeekU64 as u8,
+        Inst::PushU64 as u8,
+        1,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        0,
+        Inst::AddU64 as u8,
+        Inst::Goto as u8,
+        9,
+        0,
+        0,
+        0,  
     ]);
-    eprintln!("BYTECODE\n{}", disassemble(bytecode.remaining_slice()));
     let mut vm = Vm::new(bytecode);
     unsafe {
         vm.run();
